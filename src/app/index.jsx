701b82fbc31f3329b4dfe8b339f30d1f18e522a6
Robert Więckowski <message4robertw@gmail.com>
2025-05-19 21:00:23 +0200

    feat: Add Tailwind CSS configuration

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native";
+import tw from "../utils/tw";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Hi, welcome in my App</Text>
+    <View
+      style={tw`flex-1 items-center justify-center bg-white dark:bg-gray-900`}
+    >
+      <Text style={tw`text-xl font-bold text-gray-800 dark:text-white`}>
+        Welcome to My App
+      </Text>
     </View>
   );
 }
---
+++ src/utils/tw.js
@@ -0,0 +1,6 @@
+import { create } from "twrnc";
+
+// Create a tailwind instance with the configuration
+const tw = create(require("../../tailwind.config.js"));
+
+export default tw;
